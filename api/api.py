7c8046f82e037ee7cc79b218248655d62f2f9973
Rohan Shaw <rohnsha0@gmail.com>
2023-05-01 08:03:35 +0530

    added basic model for prdctn

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import datetime
 import yfinance as yf
 
+
+startDate= datetime.datetime.now() - datetime.timedelta(days=121)
+endDate= datetime.datetime.now() - datetime.timedelta(days=61)
+
 symbol = '^NSEI'
-data = yf.download(tickers=symbol, interval="30m", period="60d")
-data.to_csv(f'../dataset/{symbol}.csv')
+data = yf.download(tickers=symbol, interval="30m", start=startDate, end=endDate)
+with open(f'../dataset/{symbol}.csv', mode='a', newline='') as z:
+    data.to_csv(z, header=z.tell() == 0)
 
 print(f"Data retrieved & Updated for symbol: {symbol} at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+print(data.head(2))
+print(data.tail(2))
