029b11c066b014975f0b57efcc09808ed992c57b
Rohan Shaw <rohnsha0@gmail.com>
2023-05-27 10:24:26 +0530

    added instances of prediction_model into the application with some minor UI tweeks

--- StockSense/app/src/main/java/com/rohnsha/stocksense/stocksInfo.kt
+++ StockSense/app/src/main/java/com/rohnsha/stocksense/stocksInfo.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.gson.Gson
@@ -13,6 +14,10 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.ResponseBody
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 data class StockDataResponse(
     val chart: Chart
@@ -29,6 +34,7 @@ data class Result(
         val symbol: String,
         val name: String,
         val regularMarketPrice: Number,
+        val regularMarketTime: Double,
         val previousClose: Number,
         val change: Double,
         val stockVolume: Int
@@ -75,6 +81,10 @@ class stocksInfo : AppCompatActivity() {
         val ltpLay= findViewById<LinearLayout>(R.id.LTPLayout)
         val loadingTxt= findViewById<TextView>(R.id.loadingView)
         val errorTxt= findViewById<LinearLayout>(R.id.errorLayout)
+        val updationPane= findViewById<LinearLayout>(R.id.updationPane)
+        val updateLTP= findViewById<ImageView>(R.id.refreshTag)
+        val stockMarketTime= findViewById<TextView>(R.id.updationnTime)
+
 
 
 
@@ -85,16 +95,58 @@ class stocksInfo : AppCompatActivity() {
                 if (stockDataBody!=null){
                     topLay.visibility= View.VISIBLE
                     ltpLay.visibility= View.VISIBLE
+                    updationPane.visibility= View.VISIBLE
                     loadingTxt.visibility= View.GONE
 
                     stockName.text= stockDataBody.symbol
                     stockLTP.text= stockDataBody.regularMarketPrice.toString()
                     stockChange.text= (stockDataBody.regularMarketPrice.toInt()-stockDataBody.previousClose.toInt()).toString()
+                    val currentTime = Date()
+                    val formatter = SimpleDateFormat("HH:mm:ss")
+                    val formattedTime = formatter.format(currentTime)
+                    stockMarketTime.text= "Last Updated at: $formattedTime"
+
+                    updateLTP.setOnClickListener {
+                        updateStockPrice(inpSymbol)
+                    }
+                    Log.e("predictionStatus", predStats(inpSymbol).toString())
+
                 } else{
                     errorTxt.visibility= View.VISIBLE
                     loadingTxt.visibility= View.GONE
                 }
             }
         }
+    }
+
+    private fun updateStockPrice(symbol: String){
+        val stockDataFtcher= stockDataFetcher()
+        val stockMarketTime= findViewById<TextView>(R.id.updationnTime)
+        val stockLTP= findViewById<TextView>(R.id.stockPrice)
+        GlobalScope.launch(Dispatchers.IO){
+            val stockDataBody= stockDataFtcher.getStockData(symbol)
+            launch(Dispatchers.Main){
+                if(stockDataBody!=null){
+                    stockLTP.text= stockDataBody.regularMarketPrice.toString()
+                    val currentTime = Date()
+                    val formatter = SimpleDateFormat("HH:mm:ss")
+                    val formattedTime = formatter.format(currentTime)
+                    stockMarketTime.text= "Last Updated at: $formattedTime"
+                }
+            }
+        }
+    }
+
+    fun predStats(stockSymbol: String): String? {
+        val fileName= "$stockSymbol.tflite"
+        val file = File("res/data/$fileName")
+        val fileExists= file.exists()
+        var prediction: String? = null
+
+        if (fileExists){
+            prediction= "model found"
+            val length = prediction?.length
+        }
+        return prediction
     }
 }
