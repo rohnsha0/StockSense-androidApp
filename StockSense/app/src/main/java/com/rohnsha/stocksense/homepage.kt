9e8794e3b4bb5c5368b942edf191ab1f5d3b0706
Rohan Shaw <rohnsha0@gmail.com>
2023-06-09 21:38:25 +0530

    introduced basic navigation bar (no func)

--- StockSense/app/src/main/java/com/rohnsha/stocksense/homepage.kt
+++ StockSense/app/src/main/java/com/rohnsha/stocksense/homepage.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class homepage : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,8 +15,13 @@ class homepage : AppCompatActivity() {
         setContentView(R.layout.activity_homepage)
 
         val buttonSearch= findViewById<Button>(R.id.button2search)
+        val FABSearch= findViewById<FloatingActionButton>(R.id.searchBTNNav)
 
         buttonSearch.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        FABSearch.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
