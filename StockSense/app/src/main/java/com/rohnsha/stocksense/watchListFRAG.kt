a770540d32d6da9864afa941cbca20a2b0dc3c4d
Rohan Shaw <rohnsha0@gmail.com>
2023-06-28 11:20:33 +0530

    1

--- StockSense/app/src/main/java/com/rohnsha/stocksense/watchListFRAG.kt
+++ StockSense/app/src/main/java/com/rohnsha/stocksense/watchListFRAG.kt
@@ -1,6 +1,7 @@
 package com.rohnsha.stocksense
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,15 +9,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.gson.Gson
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.ResponseBody
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import watchlistDC
 
 
@@ -32,6 +31,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class watchListFRAG : Fragment() {
+
+    private val WATCHLIST_PREFS = "watchlist_prefs"
+    private val WATCHLIST_KEY = "watchlist_data"
+    private lateinit var sharedPreferences: SharedPreferences
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -74,24 +78,30 @@ class watchListFRAG : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val floatingBtnAdd= view.findViewById<FloatingActionButton>(R.id.floatingADD)
         val recyclerView= view.findViewById<RecyclerView>(R.id.rvWatchlist)
 
-        var wtchlist= mutableListOf(
-            watchlistDC("ITC.NS", "ITC Ltd."),
-            watchlistDC("INFY.NS", "Infosys Ltd."),
-            watchlistDC("KOTAKBANK.NS", "Kotak Mahindra Bank Ltd."),
-            watchlistDC("ASIANPAINT.NS", "Asian Paints Ltd."),
-            watchlistDC("BAJAJFINSV.NS", "Bajaj Finserv Ltd."),
-            watchlistDC("CIPLA.NS", "Cipla Ltd."),
-            watchlistDC("HCLTECH.NS", "HCL Technologies Ltd."),
-            watchlistDC("HDFC.NS", "HDFC Bank Ltd."),
-            watchlistDC("APOLLOHOSP.NS", "Apollo Hospitals Enterprise Ltd."),
-            watchlistDC("GRASIM.NS", "Grasim Industries Ltd.")
-        )
+        var wtchlist= mutableListOf<watchlistDC>()
 
         val adapter= watchlistAdapter(wtchlist)
         recyclerView.adapter= adapter
         recyclerView.layoutManager= LinearLayoutManager(requireContext())
+
+        floatingBtnAdd.setOnClickListener {
+            val viewInputSymbol: View= layoutInflater.inflate(R.layout.items_add, null)
+            val dialogInp= BottomSheetDialog(requireContext())
+            dialogInp.setContentView(viewInputSymbol)
+            val symboltoADD= viewInputSymbol.findViewById<EditText>(R.id.inputSymbolToADD).text.toString()
+            val btnAdd= viewInputSymbol.findViewById<Button>(R.id.addBtn)
+            val lsit= watchlistDC(symboltoADD, "ABC Ltd.")
+            btnAdd.setOnClickListener {
+                wtchlist.add(lsit)
+                adapter.notifyItemInserted(wtchlist.size-1)
+                dialogInp.dismiss()
+                Toast.makeText(requireContext(), "this is success", Toast.LENGTH_SHORT).show()
+            }
+            dialogInp.show()
+        }
 
         Log.d("watchlistsTF", wtchlist.isEmpty().toString())
         val buttonSearch = view.findViewById<Button>(R.id.button2search)
@@ -100,4 +110,25 @@ class watchListFRAG : Fragment() {
             startActivity(Intent(requireActivity(), MainActivity::class.java))
         }
     }
+
+    private fun saveWatchlistData() {
+        val gson = Gson()
+        val watchlistJson = gson.toJson(wtchlist)
+
+        sharedPreferences.edit().putString(WATCHLIST_KEY, watchlistJson).apply()
+    }
+
+    private fun loadWatchlistData() {
+        val watchlistJson = sharedPreferences.getString(WATCHLIST_KEY, null)
+
+        if (!watchlistJson.isNullOrEmpty()) {
+            val gson = Gson()
+            val type = object : TypeToken<List<watchlistDC>>() {}.type
+            wtchlist = gson.fromJson(watchlistJson, type)
+
+            // Update the RecyclerView with the loaded watchlist data
+            adapter.notifyDataSetChanged()
+        }
+    }
+
 }
